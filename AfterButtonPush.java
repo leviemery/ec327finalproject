af9abfcb6bbeebb88c5134bb073e9cf1bac21819
leviemery <33634813+leviemery@users.noreply.github.com>
2017-12-03 23:19:27 -0500

    Create AfterButtonPush.java

---
+++ AfterButtonPush.java
@@ -0,0 +1,13 @@
+//include your own package. ex. package com.example.levi.a1234;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfterButtonPush extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_button_push);
+    }
+}
