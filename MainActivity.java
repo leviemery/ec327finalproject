31b5cca08aad5dd5ccf7245ff05e941792449620
leviemery <33634813+leviemery@users.noreply.github.com>
2017-12-09 14:31:31 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,13 @@
-//include your package
+//package com.example.levi.gpacalculator; use your package instead.
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //MainActivity is the name of your java file
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button findtrainbutton = (Button) findViewById(R.id.findtrainbutton);
-        findtrainbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i1 = new Intent(MainActivity.this, AfterButtonPush.class);
-                startActivity(i1);
-            }
-        });
     }
 }
